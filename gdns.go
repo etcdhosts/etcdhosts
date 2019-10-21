a221fb07497604c207e5231cfe9927d525724659
mritd <mritd@linux.com>
2019-10-21 18:24:51 +0800

    feat(gdns): add const
    add const
    
    Signed-off-by: mritd <mritd@linux.com>

--- gdns.go
+++ gdns.go
@@ -20,6 +20,15 @@ import (
 	etcdcv3 "go.etcd.io/etcd/clientv3"
 )
 
+const (
+	GDNS_TYPE_A     = "TYPE_A"
+	GDNS_TYPE_AAAA  = "TYPE_AAAA"
+	GDNS_TYPE_TXT   = "TYPE_TXT"
+	GDNS_TYPE_CNAME = "TYPE_CNAME"
+	GDNS_TYPE_PTR   = "TYPE_PTR"
+	GDNS_TYPE_NS    = "TYPE_NS"
+)
+
 var errKeyNotFound = errors.New("key not found")
 var errQueryNotSupport = errors.New("query type not support")
 
@@ -48,17 +57,17 @@ func (gDns *GDns) getRecord(req request.Request) ([]dns.RR, error) {
 
 	switch req.QType() {
 	case dns.TypeA:
-		domainKey = path.Join(gDns.PathPrefix, domainRevers, "TYPE_A")
+		domainKey = path.Join(gDns.PathPrefix, domainRevers, GDNS_TYPE_A)
 	case dns.TypeAAAA:
-		domainKey = path.Join(gDns.PathPrefix, domainRevers, "TYPE_AAAA")
+		domainKey = path.Join(gDns.PathPrefix, domainRevers, GDNS_TYPE_AAAA)
 	case dns.TypeTXT:
-		domainKey = path.Join(gDns.PathPrefix, domainRevers, "TYPE_TXT")
+		domainKey = path.Join(gDns.PathPrefix, domainRevers, GDNS_TYPE_TXT)
 	case dns.TypeCNAME:
-		domainKey = path.Join(gDns.PathPrefix, domainRevers, "TYPE_CNAME")
+		domainKey = path.Join(gDns.PathPrefix, domainRevers, GDNS_TYPE_CNAME)
 	case dns.TypePTR:
-		domainKey = path.Join(gDns.PathPrefix, domainRevers, "TYPE_PTR")
+		domainKey = path.Join(gDns.PathPrefix, domainRevers, GDNS_TYPE_PTR)
 	case dns.TypeNS:
-		domainKey = path.Join(gDns.PathPrefix, domainRevers, "TYPE_NS")
+		domainKey = path.Join(gDns.PathPrefix, domainRevers, GDNS_TYPE_NS)
 	case dns.TypeMX:
 		fallthrough
 	case dns.TypeSRV:
