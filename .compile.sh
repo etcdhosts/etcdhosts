fc0674861b386f2294f7377cd9b36e9f0945275e
mritd <mritd@linux.com>
2020-11-16 17:49:48 +0800

    chore(compile): fix plugin build
    fix plugin build
    
    Signed-off-by: mritd <mritd@linux.com>

--- .compile.sh
+++ .compile.sh
@@ -15,6 +15,7 @@ cd ${GOPATH}/src/github.com/coredns/coredns
 git checkout tags/${VERSION} -b ${VERSION}
 go get github.com/ytpay/etcdhosts@${VERSION}
 sed -i '/^hosts:hosts/i\etcdhosts:github.com/ytpay/etcdhosts' plugin.cfg
+make -f Makefile gen
 make -f Makefile.release build tar DOCKER=coredns
 
 # copy bin file
