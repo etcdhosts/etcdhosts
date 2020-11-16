440126f6fe2c835be627bf26c6137396a7a84946
mritd <mritd@linux.com>
2020-11-16 15:00:52 +0800

    chore(build): update compile cmd
    update compile cmd
    
    Signed-off-by: mritd <mritd@linux.com>

--- .compile.sh
+++ .compile.sh
@@ -1,3 +1,22 @@
+set -ex
+
+apt update \
+  && apt upgrade -y \
+  && apt install git -y
+
+# clone source
+mkdir -p ${GOPATH}/src/github.com/coredns ${GOPATH}/src/github.com/ytpay
+git clone https://github.com/coredns/coredns.git ${GOPATH}/src/github.com/coredns/coredns
+
+# make
+cd ${GOPATH}/src/github.com/coredns/coredns
+git checkout tags/${VERSION} -b ${VERSION}
+go get github.com/ytpay/etcdhosts@${VERSION}
+sed -i '/^hosts:hosts/i\etcdhosts:github.com/ytpay/etcdhosts' plugin.cfg
+make -f Makefile.release build tar DOCKER=coredns
+
+mv release/* /build
+
 #!/usr/bin/env bash
 
 set -ex
@@ -9,16 +28,12 @@ apt update \
 # clone source
 mkdir -p ${GOPATH}/src/github.com/coredns ${GOPATH}/src/github.com/ytpay
 git clone https://github.com/coredns/coredns.git ${GOPATH}/src/github.com/coredns/coredns
-git clone https://github.com/ytpay/etcdhosts.git ${GOPATH}/src/github.com/ytpay/etcdhosts
-
-# copy plugin
-mkdir -p ${GOPATH}/src/github.com/coredns/coredns/plugin/etcdhosts
-cp ${GOPATH}/src/github.com/ytpay/etcdhosts/*.go ${GOPATH}/src/github.com/coredns/coredns/plugin/etcdhosts
 
 # make
 cd ${GOPATH}/src/github.com/coredns/coredns
 git checkout tags/${VERSION} -b ${VERSION}
-sed -i '/^hosts:hosts/i\etcdhosts:etcdhosts' plugin.cfg
-make -f Makefile gen
-make -f Makefile.release release DOCKER=coredns
+go get github.com/ytpay/etcdhosts@${VERSION}
+sed -i '/^hosts:hosts/i\etcdhosts:github.com/ytpay/etcdhosts' plugin.cfg
+make -f Makefile.release build tar DOCKER=coredns
 
+mv release/* /build
