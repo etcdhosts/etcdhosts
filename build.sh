1d4e81616c3b1030fd4ba59edf32ccd40ce44679
mritd <mritd@linux.com>
2021-02-24 18:04:25 +0800

    chore(go): update go version
    update go version
    
    Signed-off-by: mritd <mritd@linux.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-VERSION=${1:-"v1.8.0"}
+VERSION=${1:-"v1.8.1"}
 
 rm -rf build
 
 docker run --rm -it -e VERSION=${VERSION} \
                     -v `pwd`/.compile.sh:/compile.sh \
                     -v `pwd`/build:/build \
-                    golang:1.15 /compile.sh
+                    golang:1.16 /compile.sh
